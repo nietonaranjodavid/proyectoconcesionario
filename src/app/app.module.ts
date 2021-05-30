e08307d9f766a85073cc5bb50f1f6c03b8726ddb
David Nieto naranjo <davidnieto157@gmail.com>
2021-05-31 00:41:33 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
+import { VehiculosComponent } from './vehiculos/vehiculos.component';
+import { VehiculoDetailComponent } from './vehiculo-detail/vehiculo-detail.component';
+import { VehiculoService } from './vehiculo.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './app-routing.module';
+import { Grafico01Component } from './grafico01/grafico01.component';
+import { Grafico02Component } from './grafico02/grafico02.component';
+import { Grafico03Component } from './grafico03/grafico03.component';
+import { ConcesionariosComponent } from './concesionarios/concesionarios.component';
+import { ConcesionarioService } from './concesionario.service';
+import { ConcesionarioDetailComponent } from './concesionario-detail/concesionario-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    HighchartsChartModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    VehiculosComponent,
+    VehiculoDetailComponent,
+    MessagesComponent,
+    Grafico01Component,
+    Grafico02Component,
+    Grafico03Component,
+    ConcesionariosComponent,
+    ConcesionarioDetailComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [VehiculoService, MessageService, ConcesionarioService, {provide: APP_BASE_HREF, useValue: '/concesionario'}]
 })
-export class AppModule { }
+export class AppModule {}
